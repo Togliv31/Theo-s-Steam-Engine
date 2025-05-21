b723a192f97e6b33301f8a9827c0c078da52f6a0
Togliv31 <142798620+Togliv31@users.noreply.github.com>
2025-05-21 17:00:16 +0200

    Delete TheosSteamEngine directory

--- TheosSteamEngine/JS/basic.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("DOMContentLoaded", () => {
-	if (sessionStorage.getItem("Inloggad") === "true"){
-		document.getElementById("account").innerHTML = sessionStorage.getItem("user");
-		/*alert(sessionStorage.getItem("Användare"));
-		alert("Account element:" + document.getElementById("account").innerHTML);*/
-	}
-});
